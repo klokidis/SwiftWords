9f25e93ccbcd704cb6d4f9e0ac3f45e381740144
dimitrisKlokidis <dimitrisklokidis@gmail.com>
2024-07-25 03:34:55 +0300

    fix button text

--- app/src/main/java/com/example/swiftwords/ui/game.kt
+++ app/src/main/java/com/example/swiftwords/ui/game.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -31,6 +32,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.swiftwords.ui.theme.SwiftWordsTheme
@@ -60,12 +63,18 @@ fun Game(listOfLetters: List<String>) {
             CustomTextField()
             ElevatedButton(
                 onClick = {  },
+                contentPadding = PaddingValues(0.dp),
                 modifier = Modifier
                     .padding(start = 10.dp, top = 5.dp)
                     .width(70.dp)
-                    .height(50.dp)) {
-                Text("")
+                    .height(50.dp)
+            ) {
+                Text(
+                    text = "check",
+                    maxLines = 1,
+                )
             }
+
         }
 
     }
