0c32d272b1f08e33f28f1811707c1699962e7fe1
dimitrisKlokidis <dimitrisklokidis@gmail.com>
2024-07-28 22:43:50 +0300

    add level bottom bar

--- app/src/main/java/com/example/swiftwords/ui/levels/LevelScreen.kt
+++ app/src/main/java/com/example/swiftwords/ui/levels/LevelScreen.kt
@@ -211,47 +211,11 @@ fun BottomLevel(onClick: () -> Unit,level: String) {
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            Text(text = level)
+            Text(text = "Level: $level")
         }
     }
 }
 
-/*
-@Composable
-fun Level(navigateToLevel: () -> Unit,number: Int, image: Int, rightPadding: Dp, leftPadding: Dp) {
-    Box(
-        modifier = Modifier
-            .padding(
-                start = leftPadding,
-                end = rightPadding,
-                top = 50.dp
-            ),
-        contentAlignment = Alignment.Center
-    ) {
-        Image(
-            painter = painterResource(image),
-            contentDescription = null, //no need
-            modifier = Modifier
-                .size(130.dp)
-                .clickable {
-                    navigateToLevel.invoke()
-                },
-            contentScale = ContentScale.Crop
-        )
-        Text(
-            text = number.toString(),
-            style = MaterialTheme.typography.displaySmall.copy(
-                color = Color.White,
-                shadow = Shadow(
-                    color = Color.Black,
-                    offset = Offset(2f, 2f),
-                    blurRadius = 4f
-                )
-            ),
-        )
-    }
-}
-*/
 @Preview(showBackground = true)
 @Composable
 fun CardPreview() {
