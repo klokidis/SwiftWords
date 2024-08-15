660322366ed73c487de121ac6f3bf2e3b513ff08
dimitrisKlokidis <dimitrisklokidis@gmail.com>
2024-08-15 06:49:56 +0300

    add more colors

--- app/src/main/java/com/example/swiftwords/data/DataSource.kt
+++ app/src/main/java/com/example/swiftwords/data/DataSource.kt
@@ -29,14 +29,18 @@ class DataSource {
     )
 
     val colorPairs = listOf(
-        ColorPair(id = 0, lightColor = Color.White, darkColor = Color(0xFF8B0000)),
-        ColorPair(id = 1, lightColor = Color.White, darkColor = Color(0xFF006400)),
-        ColorPair(id = 2, lightColor = Color.White, darkColor = Color(0xFF092E5C)),
-        ColorPair(id = 3, lightColor = Color.White, darkColor = Color(0xFF0C9B86)),
-        ColorPair(id = 4, lightColor = Color.White, darkColor = Color(0xFF7E0A8A)),
+        ColorPair(id = 0, lightColor = Color.White, darkColor = Color(0xFF092E5C)),
+        ColorPair(id = 1, lightColor = Color.White, darkColor = Color(0xFF007FA5)),
+        ColorPair(id = 2, lightColor = Color.White, darkColor = Color(0xFF0C9B86)),
+        ColorPair(id = 3, lightColor = Color.White, darkColor = Color(0xFF7E0A8A)),
+        ColorPair(id = 4, lightColor = Color.White, darkColor = Color(0xFFC713A9)),
         ColorPair(id = 5, lightColor = Color.White, darkColor = Color(0xFFA7105E)),
-        ColorPair(id = 6, lightColor = Color.White, darkColor = Color(0xFFAC5811)),
-        ColorPair(id = 7, lightColor = Color.White, darkColor = Color(0xFF7AA710)),
+        ColorPair(id = 6, lightColor = Color.White, darkColor = Color(0xFF006400)),
+        ColorPair(id = 7, lightColor = Color.White, darkColor = Color(0xFF049C2F)),
+        ColorPair(id = 8, lightColor = Color.White, darkColor = Color(0xFF7AA710)),
+        ColorPair(id = 9, lightColor = Color.White, darkColor = Color(0xFF5E0000)),
+        ColorPair(id = 10, lightColor = Color.White, darkColor = Color(0xFFC40000)),
+        ColorPair(id = 11, lightColor = Color.White, darkColor = Color(0xFFE24615)),
     )
 
     val paddingList: List<Pair<Dp, Dp>> = listOf(
