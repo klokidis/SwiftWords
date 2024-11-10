f63b1594bf0365b2ea02fba5f55e2cb4e2c172b6
dimitrisKlokidis <dimitrisklokidis@gmail.com>
2024-11-10 11:18:23 +0200

    new colors

--- app/src/main/java/com/example/swiftwords/data/DataSource.kt
+++ app/src/main/java/com/example/swiftwords/data/DataSource.kt
@@ -8,7 +8,7 @@ import com.example.swiftwords.R
 class DataSource {
 
     val colorPairs = listOf(
-        ColorPair(id = 0, lightColor = Color.White, darkColor = Color(0xFF092E5C)),
+        ColorPair(id = 0, lightColor = Color.White, darkColor = Color(0xFF114A93)),
         ColorPair(id = 1, lightColor = Color.White, darkColor = Color(0xFF007FA5)),
         ColorPair(id = 2, lightColor = Color.White, darkColor = Color(0xFF0C9B86)),
         ColorPair(id = 3, lightColor = Color.White, darkColor = Color(0xFF7E0A8A)),
@@ -16,9 +16,9 @@ class DataSource {
         ColorPair(id = 5, lightColor = Color.White, darkColor = Color(0xFFA7105E)),
         ColorPair(id = 6, lightColor = Color.White, darkColor = Color(0xFF006400)),
         ColorPair(id = 7, lightColor = Color.White, darkColor = Color(0xFF049C2F)),
-        ColorPair(id = 8, lightColor = Color.White, darkColor = Color(0xFF7AA710)),
-        ColorPair(id = 9, lightColor = Color.White, darkColor = Color(0xFF5E0000)),
-        ColorPair(id = 10, lightColor = Color.White, darkColor = Color(0xFFC40000)),
+        ColorPair(id = 8, lightColor = Color.White, darkColor = Color(0xFF8ACE00)),
+        ColorPair(id = 9, lightColor = Color.White, darkColor = Color(0xFF940000)),
+        ColorPair(id = 10, lightColor = Color.White, darkColor = Color(0xFFDE0000)),
         ColorPair(id = 11, lightColor = Color.White, darkColor = Color(0xFFE24615)),
     )
 
