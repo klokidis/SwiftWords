1e5483bf3586f663b597e36dcd72df4cc0d876a2
dimitrisKlokidis <dimitrisklokidis@gmail.com>
2024-11-17 12:01:39 +0200

    back to version 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yukuro.swiftwords"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
